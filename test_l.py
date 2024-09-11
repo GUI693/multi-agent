7df80827ea441b25ea3c9f09aa5db6ce51efdda0
GUI693 <3146214137@qq.com>
2024-09-11 06:27:51 +0000

    new

--- test_l.py
+++ test_l.py
@@ -23,9 +23,12 @@ def reformat_table(table):
 path = "/root/mutil_agent/FREB-TQA/dataset/02_rel/wtq_nonfactoid_dev.json"
 
 
-def Response(model, tokenizer,ids, query, table):
-    for i, (id, q, t) in tqdm(enumerate(zip(ids, query, table))):
-        
+def Response(model, tokenizer,ids, query,answer, table):
+    time = 0
+    for i, (id, q,a, t) in tqdm(enumerate(zip(ids, query,answer, table))):
+        time += 1
+        if time == 4:
+            break
         table_string = reformat_table(t)
         prompt = f"Answer the question according to the table and. Return the answer following Answer:[final answer]. Table: {table_string} Question:{q}\nAnswer:"
         
@@ -36,17 +39,27 @@ def Response(model, tokenizer,ids, query, table):
         # 生成文本
         generate_ids = model.generate(
         inputs["input_ids"], 
-        max_length=500, 
+        max_length=3000, 
         num_return_sequences=1,  # 默认值就是1，这里只是为了清晰
         temperature=0.1,       # 控制生成文本的多样性
         )
 
         # 解码生成的文本
         decoded_output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-        with open('./lla.csv' 'a') as f:
-            pass
+
         print("Generated Response:")
         print(decoded_output)
+        with open('./lla.csv', 'a') as f:
+            csv_writer = csv.DictWriter(f, field)
+            csv_writer.writerow({
+                'number' : i,
+                'ids' : id,
+                'query': q,
+                'answer' : a,
+                'table': t,
+                'llm_answer' : decoded_output
+            })
+
 
 if __name__ == "__main__":
     # 加载预训练的模型和分词器
@@ -54,14 +67,12 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained("cspencergo/llama-2-7b-tabular")
 
     data = [
-        'ids', 'query', 'answer', 'table'
     ]
     
-    filed = [""]
-    with open('./lla_csv', 'w') as cs:
+    field = ['number',"ids", 'query', 'answer', 'table', 'llm_answer']
+    with open('./lla.csv', 'w') as cs:
         csv_writer = csv.DictWriter(cs, field)
         csv_writer.writeheader()
-    exit(1)
     with open(path, 'r') as f:
         data = [json.loads(line) for line in f]
 
@@ -71,6 +82,6 @@ if __name__ == "__main__":
     table = [item['table'] for item in data]
   
 
-    Response(model, tokenizer=tokenizer,ids=ids,query=query, table=table)
+    Response(model, tokenizer=tokenizer,ids=ids,query=query,answer = answer, table=table)
 
     
